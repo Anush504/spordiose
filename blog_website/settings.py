7f439fdd8312b8a7c6a9804d3c396bc480909e89
unknown <Anush5404>
2022-12-31 22:21:10 +0530

    commit

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'blog_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'qVREpba9ddEPcIAQFp3v',
+        'HOST': 'containers-us-west-62.railway.app',
+        'PORT': '7294',
     }
 }
 
