1b29d2abec3086e536ff3ce19c73b03ffb5a1fd5
unknown <Anush5404>
2023-01-20 09:47:32 +0530

    new commit

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / 'assets']
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
