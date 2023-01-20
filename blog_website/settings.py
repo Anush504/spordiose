487ebd56c926a78d8c3d8ab2edbfbc40056df100
unknown <Anush5404>
2023-01-20 09:26:07 +0530

    New Commit

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / 'assets']
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
